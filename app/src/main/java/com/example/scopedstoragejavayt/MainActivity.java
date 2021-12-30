4a3feba6d532cdc0c409e5d5cbf0cea9ee9c9d86
foxandroid <foxstream001@gmail.com>
2021-12-30 11:15:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/scopedstoragejavayt/MainActivity.java
@@ -0,0 +1,184 @@
+package com.example.scopedstoragejavayt;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.example.scopedstoragejavayt.databinding.ActivityMainBinding;
+
+import java.io.OutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private boolean isReadPermissionGranted = false;
+    private boolean isWritePermissionGranted = false;
+    ActivityResultLauncher<String[]> mPermissionResultLauncher;
+    ActivityResultLauncher<Intent> mGetImage;
+    Uri imageUri;
+    private static final int CAMERA = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mPermissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), new ActivityResultCallback<Map<String, Boolean>>() {
+            @Override
+            public void onActivityResult(Map<String, Boolean> result) {
+
+                if (result.get(Manifest.permission.READ_EXTERNAL_STORAGE) != null){
+                    isReadPermissionGranted = result.get(Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+                if (result.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) != null){
+                    isWritePermissionGranted = result.get(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                }
+
+            }
+        });
+
+        mGetImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null){
+
+                    Bundle bundle = result.getData().getExtras();
+                    Bitmap bitmap = (Bitmap) bundle.get("data");
+
+                    if (isWritePermissionGranted){
+
+                        if (saveImageToExternalStorage(UUID.randomUUID().toString(),bitmap)){
+
+                            Toast.makeText(MainActivity.this,"saved Image Successfully" ,Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }else {
+
+                        Toast.makeText(MainActivity.this,"Permission not granted" ,Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                }
+
+            }
+        });
+
+        requestPermission();
+
+        binding.saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetImage.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+
+            }
+        });
+    }
+
+    private void requestPermission() {
+
+
+        boolean minSDK = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q;
+
+        isReadPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED;
+
+        isWritePermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED;
+
+        isWritePermissionGranted = isWritePermissionGranted || minSDK;
+
+        List<String> permissionRequest = new ArrayList<String>();
+
+        if (!isReadPermissionGranted){
+
+            permissionRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        }
+        if (!isWritePermissionGranted){
+
+            permissionRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionRequest.isEmpty()){
+
+            mPermissionResultLauncher.launch(permissionRequest.toArray(new String[0]));
+        }
+
+    }
+
+    private boolean saveImageToExternalStorage(String imgName, Bitmap bmp){
+
+        Uri ImageCollection = null;
+        ContentResolver resolver = getContentResolver();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+
+            ImageCollection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+
+        }else {
+
+            ImageCollection = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imgName + ".jpg");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg");
+        Uri imageUri = resolver.insert(ImageCollection, contentValues);
+
+        try {
+
+            OutputStream outputStream = resolver.openOutputStream(Objects.requireNonNull(imageUri));
+            bmp.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+            Objects.requireNonNull(outputStream);
+            return true;
+
+        }
+        catch (Exception e){
+
+            Toast.makeText(this,"Image not saved: \n" + e,Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+
+        }
+
+        return false;
+
+
+    }
+
+}
